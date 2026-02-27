b56d97104449ff3bcdd5d5942e4b65b298c977c7
David Elias Moriello <davideliasmoriello@gmail.com>
2026-02-27 11:22:46 -0300

    COnexion a BD y ajustes en el nombre

--- leban_challenge/src/main/java/com/challenge/leban/service/DepartamentoServiceImpl.java
+++ leban_challenge/src/main/java/com/challenge/leban/service/DepartamentoServiceImpl.java
@@ -1,13 +1,16 @@
 package com.challenge.leban.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.challenge.leban.dto.DepartamentoDto;
 import com.challenge.leban.entity.Departamento;
 import com.challenge.leban.repository.IDepartamentoRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 @Log4j2
+@Service
 public class DepartamentoServiceImpl implements IDepartamentoService {
 
     private IDepartamentoRepository departamentoRepository;
@@ -39,8 +42,8 @@ public class DepartamentoServiceImpl implements IDepartamentoService {
     public List<DepartamentoDto> filterDepartamentos(String disponible, String precioMin, String precioMax) {
 
         Boolean disp = (disponible != null && !disponible.isEmpty()) ? Boolean.valueOf(disponible) : null;
-        Double pMin = (precioMin != null && !precioMin.isEmpty()) ? Double.valueOf(precioMin) : null;
-        Double pMax = (precioMax != null && !precioMax.isEmpty()) ? Double.valueOf(precioMax) : null;
+        BigDecimal pMin = (precioMin != null && !precioMin.isEmpty()) ? BigDecimal.valueOf(Double.parseDouble(precioMin))  : null;
+        BigDecimal pMax = (precioMax != null && !precioMax.isEmpty()) ? BigDecimal.valueOf(Double.parseDouble(precioMax)) : null;
 
         return departamentoRepository.findWithFilters(disp, pMin, pMax)
                 .stream().map(Departamento::getDTO).toList();
